1adc89441f81b9b8215c34e9017f70f9e9693bad
maxxtron <maxxtron93@gmail.com>
2025-02-03 16:08:50 +0200

    start

--- src/app/components/Header/Logo/Logo.tsx
+++ src/app/components/Header/Logo/Logo.tsx
@@ -1,10 +1,13 @@
 import Image from "next/image"
+import Link from "next/link"
 
 
 const Logo = () => {
   return (
     <div>
+        <Link href={'/'}>
       <Image src="/img/logo.png" alt="Laser Master" width={168} height={54}/>
+        </Link>
     </div>
   )
 }
