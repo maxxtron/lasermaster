8665840caafeb7d9ced14b0f034cc05b3d370d6c
Prokopenko Mykyta <maxxtron93@gmail.com>
2025-02-04 07:05:27 +0200

    add up nav

--- src/app/components/Header/Header.tsx
+++ src/app/components/Header/Header.tsx
@@ -1,11 +1,13 @@
 import s from './header.module.scss'
 import Logo from './Logo/Logo'
+import Nav from './Nav/Nav'
 
 const Header = () => {
   return (
     <header className={s.header}>
         <div className={s.container}>
             <Logo />
+            <Nav />
         </div>
     </header>
   )
---
+++ src/app/components/Header/Nav/Nav.tsx
@@ -0,0 +1,29 @@
+import Link from 'next/link'
+import s from './nav.module.scss'
+import Image from "next/image"
+
+const Nav = () => {
+  return (
+    <nav className={s.container}>
+      <div className={s.up_level}>
+            <ul className={s.list}>
+                <li className={s.info}>м. Київ (Борщагівка), вул. Івана Дзюби 9</li>
+                <li className={s.info}>Пн-Пт: 09:00–18:00</li>
+                <li className={s.phone}>
+                    <Link href='tel:+380985035679' className={s.link_phone}>
+                        <Image src='/icons/phone.png' alt='Телефон' width={24} height={24} />
+                        <span>+380 97 854 24 01</span>
+                    </Link>
+                </li>
+                <li className={s.telegram}>
+                    <Link href='https://telegram.me/ra_alex_888'>
+                        <Image src='/icons/telegram.png' alt='Телеграм' width={40} height={40} />
+                    </Link>
+                </li>
+            </ul>
+      </div>
+    </nav>
+  )
+}
+
+export default Nav
