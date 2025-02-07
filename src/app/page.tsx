ee542fd2a3506cfd6ea3b08dc3797a8b92f817a6
Prokopenko Mykyta <maxxtron93@gmail.com>
2025-02-07 07:25:06 +0200

    add main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Header from "./components/Header/Header";
+import OurProduct from "./components/OurProduct/OurProduct";
 
 export default function Home() {
   return (
     <div>
       <Header />
+      <OurProduct/>
     </div>
   );
 }
