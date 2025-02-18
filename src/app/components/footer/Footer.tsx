e5f43686ca0330c7834ecada6f32053cba118f3a
maxxtron <maxxtron93@gmail.com>
2025-02-18 16:36:32 +0200

    add footer

---
+++ src/app/components/footer/Footer.tsx
@@ -0,0 +1,11 @@
+import s from './footer.module.scss'
+
+const Footer = () => {
+  return (
+    <footer className={s.container}>
+      <span>Copyright  2025 Lasermaster</span>
+    </footer>
+  )
+}
+
+export default Footer
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,5 @@
 'use client'
+import Footer from "./components/footer/Footer";
 import Header from "./components/Header/Header";
 import OurProduct from "./components/OurProduct/OurProduct";
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
     <div>
       <Header />
       <OurProduct/>
+      <Footer />
     </div>
   );
 }
