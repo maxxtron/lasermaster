1ecdd399b370beb67beb6803b1dffd1912d027b8
maxxtron <maxxtron93@gmail.com>
2025-02-25 09:37:43 +0200

    fix

--- src/app/components/resuable/buttons/ButtonWithPopup.tsx
+++ src/app/components/resuable/buttons/ButtonWithPopup.tsx
@@ -2,7 +2,6 @@ import React, { useState } from 'react';
 import s from './buttonWithPopup.module.scss';
 import Popup from '../popup/Popup';
 import PopupCall from '../../OurProduct/PopupCall';
-import Image from 'next/image';
 
 const ButtonWithPopup = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
@@ -12,12 +11,12 @@ const ButtonWithPopup = () => {
   };
 
   return (
-    <button className={s.roundButton} onClick={togglePopup}>
-      {/* <Image src="/icons/phonebtn.svg" width={40} height={40} alt="call back" className={s.button_call}/> */}
+    <>
+      <button className={s.roundButton} onClick={togglePopup} />
       <Popup isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <PopupCall onClose={() => setIsOpen(false)} />
       </Popup>
-    </button>
+    </>
   );
 };
 
