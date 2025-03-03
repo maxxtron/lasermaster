c4f2ba1cba6ea6696dca726ca20b185feeadbb6d
maxxtron <maxxtron93@gmail.com>
2025-03-03 15:42:31 +0200

    fix btn

--- src/app/components/resuable/buttons/ButtonWithPopup.tsx
+++ src/app/components/resuable/buttons/ButtonWithPopup.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 import s from './buttonWithPopup.module.scss';
 import Popup from '../popups/Popup';
 import PopupCall from '../popups/PopupCall';
@@ -6,13 +6,30 @@ import PopupCall from '../popups/PopupCall';
 const ButtonWithPopup: React.FC = () => {
   const [isOpen, setIsOpen] = useState<boolean>(false);
 
+  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      const header = document.querySelector('header');
+      if (header) {
+        const headerRect = header.getBoundingClientRect();
+        setIsHeaderVisible(headerRect.bottom > 0);
+      }
+    };
+
+    window.addEventListener('scroll', handleScroll);
+    return () => {
+      window.removeEventListener('scroll', handleScroll);
+    };
+  }, []);
+
   const togglePopup = () => {
     setIsOpen(!isOpen);
   };
 
   return (
     <>
-      <button className={s.roundButton} onClick={togglePopup} />
+      <button className={!isHeaderVisible ? s.roundButton : ''} onClick={togglePopup} />
       <Popup isOpen={isOpen} onClose={() => setIsOpen(false)}>
         <PopupCall onClose={() => setIsOpen(false)} />
       </Popup>
