302668bcaca0764197c3997eb1cdee3d51eac98d
maxxtron <maxxtron93@gmail.com>
2025-03-06 15:26:34 +0200

    fix tg btn

--- src/app/components/resuable/buttons/TelegramButton.tsx
+++ src/app/components/resuable/buttons/TelegramButton.tsx
@@ -1,42 +1,13 @@
-import React, { useEffect, useState } from 'react';
 import s from './telegramButton.module.scss';
+import Link from 'next/link';
 
 const ButtonWithPopup: React.FC = () => {
-  const [isOpen, setIsOpen] = useState<boolean>(false);
-  const [isHeaderVisible, setIsHeaderVisible] = useState(true);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const header = document.querySelector('header');
-      if (header) {
-        const headerRect = header.getBoundingClientRect();
-        setIsHeaderVisible(headerRect.bottom > 0);
-      }
-    };
-
-    window.addEventListener('scroll', handleScroll);
-    return () => {
-      window.removeEventListener('scroll', handleScroll);
-    };
-  }, []);
-
-  const togglePopup = () => {
-    setIsOpen(!isOpen);
-  };
-
   return (
-    <>
-      <button
-        className={`${s.telegramButton} ${!isHeaderVisible ? s.visible : ''}`}
-        onClick={togglePopup}
-      />
-      <a
-        href="https://telegram.me/ra_alex_888"
-        target="_blank"
-        rel="noopener noreferrer"
-        // className={!isHeaderVisible ? s.telegramButton : ''}
-      />
-    </>
+    <Link
+      href="https://telegram.me/ra_alex_888"
+      target="_blank"
+      className={s.telegramButton}
+    ></Link>
   );
 };
 
