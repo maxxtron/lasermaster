cdc529c02697f9743a6d6f3cda99e0ca7d81f5fc
maxxtron <maxxtron93@gmail.com>
2025-03-06 17:52:25 +0200

    fix addaptive

--- src/app/components/Header/Logo/Logo.tsx
+++ src/app/components/Header/Logo/Logo.tsx
@@ -7,13 +7,13 @@ const Logo = () => {
     <div>
       <Link href="/" passHref>
         <Image src="/img/logo.png" alt="Laser Master" width={168} height={54} className={s.logo} />
-        <Image
+        {/* <Image
           src="/img/logo_small.png"
           alt="Laser Master"
           width={160}
           height={40}
           className={s.logo_small}
-        />
+        /> */}
       </Link>
     </div>
   );
