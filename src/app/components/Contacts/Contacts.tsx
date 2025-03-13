062cd7f8bce19349ed1ecb05f92e0b54b9832fe6
maxxtron <maxxtron93@gmail.com>
2025-03-13 10:54:57 +0200

    update

--- src/app/components/Contacts/Contacts.tsx
+++ src/app/components/Contacts/Contacts.tsx
@@ -4,7 +4,7 @@ const Contacts = () => {
   return (
     <section className={s.map_container}>
       <iframe
-        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d383.8717381126843!2d30.398080012076093!3d50.4223123261069!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cbf1eeff110b%3A0x6b8aa1e160586574!2z0L_RgNC-0YHQv9C10LrRgiDQkNC60LDQtNC10LzRltC60LAg0JrQvtGA0L7Qu9GM0L7QstCwLCA5LCDQmtC40ZfQsiwgMDIwMDA!5e0!3m2!1suk!2sua!4v1741855878018!5m2!1suk!2sua"
+        src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d2542.0277673169962!2d30.39746164293769!3d50.42195428316846!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x40d4cbf1eeff110b%3A0x6b8aa1e160586574!2z0L_RgNC-0YHQv9C10LrRgiDQkNC60LDQtNC10LzRltC60LAg0JrQvtGA0L7Qu9GM0L7QstCwLCA5LCDQmtC40ZfQsiwgMDIwMDA!5e0!3m2!1suk!2sua!4v1741856038836!5m2!1suk!2sua"
         width="100%"
         height="600"
         style={{ border: 0 }}
